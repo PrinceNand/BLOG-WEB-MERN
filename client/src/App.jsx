c3699fd5779b4330bdd01c2497b1ffb4036458f3
Prince <ashnand2000@gmail.com>
2024-10-12 13:11:46 +0530

    3.Header: Logo created and Flowbite installed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,13 @@ import Projects from "./pages/Projects";
 import Dashboard from "./pages/Dashboard";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      {/* Add Header */}
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<About />} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import { Navbar } from "flowbite-react";
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+          Ash's
+        </span>{" "}
+        Blog
+      </Link>
+    </Navbar>
+  );
+}
+
+export default Header;
