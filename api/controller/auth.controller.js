e0b3d77aaa9d4be9c5676fe5c3edd2ff48aad8e4
Prince <ashnand2000@gmail.com>
2024-10-13 19:01:52 +0530

    20.Routes: AuthRoutes and Controller for SignIn Page | jwt to generate token

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,7 +1,9 @@
 import User from "../model/user.model.js";
 import bycrptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
+// Auth Routes for Sign Up
 export const signup = async (req, res, next) => {
   // get data from json post req
   const { username, email, password } = req.body;
@@ -38,5 +40,42 @@ export const signup = async (req, res, next) => {
 
     // using middleware
     next(error);
+  }
+};
+
+// Auth Routes for Sign In
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All feilds are required!"));
+  }
+
+  try {
+    // find user in db
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) {
+      next(404, "User Not Found!");
+    }
+
+    // check if password is matching with user
+    const validPassword = bycrptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(400, "Invalid Credentials!");
+    }
+
+    // using jwt to get convert the authentication to token
+    // validUser._id: to get the unique id created in db for user | JWT_SECRET: to hide the token
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    // hide the password so that it should not get send to user
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(rest);
+  } catch (e) {
+    next(e);
   }
 };
