649ab17e847631e38d1f8e5e319c1a3a0ecc843a
Prince <ashnand2000@gmail.com>
2024-10-24 20:17:22 +0530

    35.Delete-User: Api Routes / Controller

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -2,10 +2,12 @@ import express from "express";
 import { test } from "../controller/user.controller.js";
 import { updateUser } from "../controller/user.controller.js";
 import { verifyToken } from "../utils/verify.user.js";
+import { deleteUser } from "../controller/user.controller.js";
 
 const router = express.Router();
 
 router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser);
+router.delete("/delete/:userId", verifyToken, deleteUser);
 
 export default router;
