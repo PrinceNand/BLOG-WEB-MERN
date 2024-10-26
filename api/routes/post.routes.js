407297afaefec666103a86d9b066199cccd6792e
Prince <ashnand2000@gmail.com>
2024-10-26 14:45:46 +0530

    48.PostSection: Add Delete Functionality in post

--- api/routes/post.routes.js
+++ api/routes/post.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { verifyToken } from "../utils/verify.user.js";
-import { create, getposts } from "../controller/post.controller.js";
+import { create, deletePost, getposts } from "../controller/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
 router.get("/getposts", getposts);
+router.delete("/deletepost/:postId/:userId", verifyToken, deletePost);
 
 export default router;
