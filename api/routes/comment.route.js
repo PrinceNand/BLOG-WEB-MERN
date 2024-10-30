75713455de6b0ea54a29d6e90bdb4f268a69da21
prince <ashnand2000@gmail.com>
2024-10-30 19:54:03 +0530

    61.Comment-Section: Show the added comment for post based on user

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { verifyToken } from "../utils/verify.user.js";
 import { createComment } from "../controller/comment.controller.js";
+import { getPostComments } from "../controller/comment.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createComment);
+router.get("/getPostComments/:postId", getPostComments);
 
 export default router;
--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -5,6 +5,7 @@ import { verifyToken } from "../utils/verify.user.js";
 import { deleteUser } from "../controller/user.controller.js";
 import { signOutUser } from "../controller/user.controller.js";
 import { getUsers } from "../controller/user.controller.js";
+import { getUser } from "../controller/user.controller.js";
 
 const router = express.Router();
 
@@ -13,5 +14,6 @@ router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", verifyToken, signOutUser);
 router.get("/getusers", verifyToken, getUsers);
+router.get("/:userId", getUser);
 
 export default router;
