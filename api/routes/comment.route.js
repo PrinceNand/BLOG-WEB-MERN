ab6e47493380e20892d57c7cb2a0899f4c4a5e1a
prince <ashnand2000@gmail.com>
2024-10-30 20:17:13 +0530

    64.Comment: Add delete functionaltiy to comment and also admin can delete it

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -4,6 +4,7 @@ import { createComment } from "../controller/comment.controller.js";
 import { getPostComments } from "../controller/comment.controller.js";
 import { likeComment } from "../controller/comment.controller.js";
 import { editComment } from "../controller/comment.controller.js";
+import { deleteComment } from "../controller/comment.controller.js";
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.post("/create", verifyToken, createComment);
 router.get("/getPostComments/:postId", getPostComments);
 router.put("/likeComment/:commentId", verifyToken, likeComment);
 router.put("/editComment/:commentId", verifyToken, editComment);
+router.delete("/deleteComment/:commentId", verifyToken, deleteComment);
 
 export default router;
